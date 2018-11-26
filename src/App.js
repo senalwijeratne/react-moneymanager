c963b3169542cfb69d40274d65066c21995c386a
Senal Wijeratne <senal.ra128@gmail.com>
2018-11-26 21:25:27 +0530

    added styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  text-align: center;
+
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 40vmin;
+  }
+
+  .App-link {
+    color: #61dafb;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+const Heading = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <AppContainer>
+        <Heading>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,8 +52,8 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
-      </div>
+        </Heading>
+      </AppContainer>
     )
   }
 }
